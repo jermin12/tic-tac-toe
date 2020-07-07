f95d31541ed4a769cfbf2b39347715a0d611d5ff
jermin12 <jermin.kamel@gmail.com>
2020-07-07 15:40:39 +0800

    Updates

---
+++ precache-manifest.f9b323ad7e99c40ae1c584a27187b8c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "152ec3ef25ce5522241b821ebf48497b",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "649baaf1c4803d4ffcf8",
+    "url": "/tic-tac-toe/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9ef516924bbe38c3f10d",
+    "url": "/tic-tac-toe/static/css/main.0391e916.chunk.css"
+  },
+  {
+    "revision": "649baaf1c4803d4ffcf8",
+    "url": "/tic-tac-toe/static/js/2.ecbe52fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.ecbe52fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ef516924bbe38c3f10d",
+    "url": "/tic-tac-toe/static/js/main.0f081cbf.chunk.js"
+  },
+  {
+    "revision": "5df9288f01178d7dbd16",
+    "url": "/tic-tac-toe/static/js/runtime-main.601deabe.js"
+  }
+]);
